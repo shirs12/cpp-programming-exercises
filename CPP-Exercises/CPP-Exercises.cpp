e94491aeb7993821d83c04f83b135a6833f45057
shirs12 <shirseg98@gmail.com>
2023-02-01 18:05:07 +0200

    Add project files.

---
+++ CPP-Exercises/CPP-Exercises.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+#include<math.h>
+
+int main() {
+	// 1
+	cout << "Hello World!\n" << endl;
+
+	// 2
+	int nFirstNum;
+	int nSecondNum;
+
+	cout << "Enter first number: ";
+	cin >> nFirstNum;
+
+	cout << "\nEnter second number: ";
+	cin >> nSecondNum;
+
+	if (cin.fail())
+	{
+		cin.clear(); // clears the stream
+		cout << -1;
+	}
+	else {
+
+		cout << "\nSum = " << nFirstNum + nSecondNum << "\n";
+
+		cout << "Sub = " << abs(nFirstNum - nSecondNum) << "\n";
+
+		cout << "Mul = " << nFirstNum * nSecondNum << "\n";
+
+		cout << "Sub = " << nFirstNum % nSecondNum << "\n" << endl;
+	}
+
+	// 3
+	int nHeight;
+	int nWidth;
+
+	cout << "Enter height: ";
+	cin >> nHeight;
+
+	cout << "\nEnter width: ";
+	cin >> nWidth;
+
+	if (cin.fail())
+	{
+		cin.clear(); // clears the stream
+		cout << -1;
+	}
+	else {
+		cout << "\nArea: " << nHeight * nWidth;
+
+		cout << "\nPerimeter: " << (nHeight * 2) + (nWidth * 2);
+	}
+
+	// 4
+	int nFirstNumber;
+	int nSecondNumber;
+	int nThirdNumber;
+
+	cout << "\nEnter first number: ";
+	cin >> nFirstNumber;
+
+	cout << "\nEnter second number: ";
+	cin >> nSecondNumber;
+
+	cout << "\nEnter third number: ";
+	cin >> nThirdNumber;
+
+	if (cin.fail())
+	{
+		cin.clear();
+		cout << -1;
+	}
+	else {
+		cout << "Average: " << (nFirstNumber + nSecondNumber + nThirdNumber) / 3;
+	}
+
+	// 5
+
+
+
+
+}
