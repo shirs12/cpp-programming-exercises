f81699c549bc1e46a678a7d71b3d8329c89de0b9
shirs12 <shirseg98@gmail.com>
2023-02-07 19:12:54 +0200

    update

--- CPP-Exercises03/Source.cpp
+++ CPP-Exercises03/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void evenNumbers() {
-	for (int i = 0; i < 101; i+=2)
+	for (int i = 0; i < 101; i += 2)
 	{
 		cout << i << endl;
 	}
@@ -168,6 +168,160 @@ void printSquare() {
 	}
 }
 
+void printChessBoard() {
+	int nNumber;
+
+	cout << "Enter a number: ";
+	cin >> nNumber;
+
+	for (int i = 0; i < nNumber; i++)
+	{
+		for (int j = 0; j < nNumber; j++)
+		{
+			if ((i + j) % 2 == 0)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+void calcSum2() {
+	int nNumber;
+	int nSum = 0;
+
+	cout << "Enter numbers (-999 to stop): " << endl;
+	cin >> nNumber;
+
+	while (nNumber != -999)
+	{
+		nSum += nNumber;
+		cin >> nNumber;
+	}
+	cout << "Sum: " << nSum << endl;
+}
+
+void calcDigits() {
+	int nNumber;
+	int nSum = 0;
+
+	cout << "Enter a number greater than 100: ";
+	cin >> nNumber;
+
+	if (nNumber < 100)
+	{
+		cout << "Error! input must be greater than 100" << endl;
+	}
+	else
+	{
+		while (nNumber > 0)
+		{
+			nSum += nNumber % 10;
+			nNumber /= 10;
+		}
+		cout << nSum << endl;
+	}
+
+}
+
+void avgNumbers() {
+	int nNumber = 0;
+	int nCountPositive = 0;
+	int nCountNegative = 0;
+	int nSum = 0;
+
+	cout << "Enter numbers until there are three times negative numbers "
+		"enterd more than positive : " << endl;
+
+	while ((nCountPositive != (nCountNegative / 3)) || nCountPositive == 0 || nCountNegative == 0)
+	{
+		cin >> nNumber;
+		if (nNumber > 0)
+		{
+			nCountPositive++;
+		}
+		else if (nNumber < 0)
+		{
+			nCountNegative++;
+		}
+
+		nSum += nNumber;
+	}
+
+	cout << "Average: " << (double)(nSum / (nCountPositive + nCountNegative));
+}
+
+//int reverseNum(int nNumber) {
+//	string result;
+//
+//	while (nNumber != 0)
+//	{
+//		
+//	}
+//
+//	return result;
+//}
+
+void methodsMenu() {
+	int nChoice = 0;
+	int nNumber;
+	int nFirstNumber;
+	int nSecondNumber;
+
+	while (nChoice != 3)
+	{
+		cout << "\nMenu:"
+			"\n1. to reverse order of digits"
+			"\n2. to append number to another"
+			"\n3. Exit"
+			"\nEnter your choice: ";
+		cin >> nChoice;
+
+		if (nChoice > 3 || nChoice < 1)
+		{
+			cout << "Wrong choice." << endl;
+		}
+
+		switch (nChoice)
+		{
+		case(1):
+			cout << "Enter a number: ";
+			cin >> nNumber;
+
+			cout << "Reversed: ";
+			while (nNumber != 0)
+			{
+				cout << nNumber % 10;
+				nNumber /= 10;
+			}
+			cout << endl;
+
+			break;
+
+		case(2):
+			cout << "Enter two numbers: " << endl;
+			cin >> nFirstNumber;
+			cin >> nSecondNumber;
+
+			cout << "Appended: " << nFirstNumber << nSecondNumber << endl;
+
+			break;
+
+		case(3):
+
+			break;
+
+		default:
+			break;
+		}
+	}
+}
+
 int main() {
 	// 23
 	//evenNumbers();
@@ -192,4 +346,20 @@ int main() {
 
 	// 30
 	//printSquare();
+
+	// 31
+	//printChessBoard();
+
+	// 32
+	//calcSum2();
+
+	// 33
+	//calcDigits();
+
+	// 34
+	//avgNumbers();
+
+	// 35
+	methodsMenu();
 }
+
